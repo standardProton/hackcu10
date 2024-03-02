bf6ea3e7996f8e86fd24542cdbd3c703b7a2bf27
Alex M <alexm024680@gmail.com>
2024-03-02 16:29:49 -0700

    Finished sine wave

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,27 +2,34 @@ import Image from "next/image";
 import page from "../app/css/main_page.module.css";
 import styles from "../app/css/main.module.css";
 import globals from "../app/css/globals.css";
+import { Button } from "@mui/material";
+import Head from "next/head";
 
 import { useState } from "react";
 import { useEffect } from "react";
 
 export default function Home() {
 
-  useEffect(() => {
+  /*useEffect(() => {
     if (document == undefined) return;
+    
+    const sine = document.getElementById("sine-img");
+    setInterval(() => {
+      
+      sine.style.top = "" + (parseInt(sine.style.top) - 5) + "px";
+    }, 10)
+  });*/
 
-    const sine_svg = document.getElementById("sine-wave-svg");
-
-
-  });
-
-  return (
+  return (<>
+    <Head>
+      <title>CU Scholarships</title>
+    </Head>
     <div className={page.main}>
       <div className={page.header}>
-        <div style={{top: "0", width: "100%", height: "20px", backgroundColor: "#F0F"}}>
-          <svg id="sine-wave-svg" height="100%" width="100%"></svg>
-        </div>
         <div className={page.header_gradient}></div>
+        <div className={page.sine_img} id="sine-img">
+          <Image src="/sine.png" width="1920" height="50" alt="Sine"></Image>
+        </div>
         <div className={page.header_menu_container}>
           <div className={styles.flex + " " + styles.hide_on_mobile}  style={{justifyContent: "right", gap: "10px"}}>
             <a href="https://google.com/">
@@ -43,9 +50,12 @@ export default function Home() {
         <div className={page.signup_container}>
             <div className={styles.box + " " + styles.margin_auto} style={{width: "35%", height: "300px"}}>
               <h1 style={{letterSpacing: "1px", fontSize: "40pt"}}>Find the perfect scholarship</h1>
+              <div style={{marginTop: "50px"}}>
+                <Button variant="contained" style={{backgroundColor: "#CFB87C", minWidth: "200px", maxWidth: "200px", minHeight: "61px", maxHeight: "61px", fontSize: "14pt"}}>Get Started</Button>
+              </div>
             </div>
         </div>
       </div>
     </div>
-  );
+  </>);
 }
