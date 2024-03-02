ae088d84e2a5cbabbca05b4f885e1a22ae30be0a
Alex M <alexm024680@gmail.com>
2024-03-02 12:23:49 -0700

    Initial UI on homepage

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,8 +1,21 @@
 import Image from "next/image";
-import styles from "./page.module.css";
+import styles from "../app/page.module.css";
+import globals from "../app/globals.css";
+
 
 export default function Home() {
   return (
-    <span>Hello World</span>
+    <div className={styles.main}>
+      <div className={styles.header}>
+
+      </div>
+      <div className={styles.container}>
+        <div className={styles.signup_container}>
+          <div className={styles.box}>
+            <h1>Find the perfect scholarship</h1>
+          </div>
+        </div>
+      </div>
+    </div>
   );
 }
